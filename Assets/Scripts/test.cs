392c18e23e94ef7d6a92e77b470045e7faca8dcb
Umut <64823238+BayPatates@users.noreply.github.com>
2023-12-28 16:19:03 +0300

    Update test.cs

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class test : MonoBehaviour
 {
+    public int maxAmmo = 150;
+    public int totalAmmo = 90;
+    public int magazineCapacity = 30;
+    public int ammoInMagazine = 30;
+    public int pickUpAmount = 30;
+    public int shootingDelayInMS = 100;
+    bool canShoot = true;
+    int fixedTimer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +24,32 @@ public class test : MonoBehaviour
     {
         
     }
+
+    public void pickUpAmmo() {
+        totalAmmo = Mathf.Min((totalAmmo+pickUpAmount), maxAmmo);
+    }
+
+    public void fire() {
+        if (canShoot == true && ammoInMagazine > 0) {
+            ammoInMagazine -= 1;
+            fixedTimer = 0;
+            canShoot = false;
+        }
+    }
+
+    void FixedUpdate() {
+        fixedTimer += 1;
+        if (fixedTimer>3) {
+            canShoot = true;
+        }
+    }
+
+    public void reloadGun() {
+        if (totalAmmo<1) {print("Not enough ammo."); return;}
+        int neededAmmoAmount = Mathf.Min(magazineCapacity - ammoInMagazine, totalAmmo);
+        if (neededAmmoAmount < 1) {print("No need for reload."); return;}
+        ammoInMagazine += neededAmmoAmount;
+        totalAmmo -= neededAmmoAmount;
+    }
+
 }
